396458ae93a57585811b58560b53bc00961ba6a5
samuel9eun <samuel9eun@gmail.com>
2017-01-28 13:23:46 -0500

    flashcard app

---
+++ app.js
@@ -0,0 +1,13 @@
+
+var operand1;
+var operand2;
+
+function getNumber(){
+  //test
+  alert("test");
+  operand1=Math.floor(Math.random()*20)+1;
+  operand2=Math.floor(Math.random()*20)+1;
+  document.getElementById("operand1").innerHTML=operand1;
+  document.getElementById("operand2").innerHTML=operand2;
+  document.getElementById("results").innerHTML="";
+}
