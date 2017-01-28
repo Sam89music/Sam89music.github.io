f3db5b15520e4930812f9ecaec7b13a8788937d2
samuel9eun <samuel9eun@gmail.com>
2017-01-28 13:28:37 -0500

    flashcard app

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ var operand1;
 var operand2;
 
 function getNumber(){
-  //test
-  alert("test");
   operand1=Math.floor(Math.random()*20)+1;
   operand2=Math.floor(Math.random()*20)+1;
   document.getElementById("operand1").innerHTML=operand1;
   document.getElementById("operand2").innerHTML=operand2;
   document.getElementById("results").innerHTML="";
+}
+
+function checkAnswer(){
+  var userSum = document.getElementById("userAnswer").value;
+  var sum = operand1 + operand2;
 }
