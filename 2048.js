b3b0d5491cfd2cfe33b19ffb810bd8829f5f361b
samuel9eun <samuel9eun@gmail.com>
2017-04-15 14:00:14 -0400

    finished bootleg 2048 just need to fix spawning

--- 2048.js
+++ 2048.js
@@ -44,10 +44,16 @@ function spawnPlace(){
       firstTime = true;
     
   }
+  //spawns it the second time
   else{
       globalRow2 = row;
       globalCol2 = col;
   }
+}
+
+//get random numbers
+function getRandomNum(min, max){
+  return Math.floor(Math.random() * max) + min;
 }
 
 //detects button pressed
@@ -58,18 +64,28 @@ function buttons(){
     moveUp();
     moveUp();
     moveUp();
+    spawnNumberAfterTurn();
   }
   if (keycode == 40){
     //alert ("down");
     moveDown();
     moveDown();
     moveDown();
+    spawnNumberAfterTurn();
   }
   if (keycode == 39){
-    alert ("right");
+    //alert ("right");
+    moveRight();
+    moveRight();
+    moveRight();
+    spawnNumberAfterTurn();
   }
   if (keycode == 37){
-    alert ("left");
+    //alert ("left");
+    moveLeft();
+    moveLeft();
+    moveLeft();
+    spawnNumberAfterTurn();
   }
 }
 
@@ -80,15 +96,30 @@ function moveUp(){
   while(canMove){
     for(var j = 1; j < 4; j++){
       for(var i = 1; i <= 4; i++){
+        
+        //element of focus
         var element = document.getElementById("row" + j + "col" + i).innerHTML;
-
+        var elementBelow = document.getElementById("row" + (j + 1) + "col" + i).innerHTML;
+        
+      //if there is nothing inside element, move the one below up
       if(element === ""){
-          var elementBelow = document.getElementById("row" + (j + 1) + "col" + i).innerHTML;
+          
           document.getElementById("row" + j + "col" + i).innerHTML = elementBelow;
-          document.getElementById("row" + (j +1) + "col" + i).innerHTML = "";
+          document.getElementById("row" + (j + 1) + "col" + i).innerHTML = "";
         }
         else{
           canMove = false;
+          
+          //if the numbers are the same then add them
+          if(element === elementBelow){
+            //add elements because they match
+            var sum = parseInt(element) + parseInt(elementBelow);
+            //put sum into the element above
+            document.getElementById("row" + j + "col" + i).innerHTML=sum;
+            //change element below with empty string
+            document.getElementById("row" + (j + 1) + "col" + i).innerHTML="";
+            
+          }
         }
       }
     }
@@ -103,22 +134,114 @@ function moveDown(){
     for(var j = 4; j > 1; j--){
       for(var i = 4; i >= 1; i--){
         var element = document.getElementById("row" + j + "col" + i).innerHTML;
+        var elementBelow = document.getElementById("row" + (j - 1) + "col" + i).innerHTML;
 
       if(element === ""){
-          var elementBelow = document.getElementById("row" + (j - 1) + "col" + i).innerHTML;
+          
           document.getElementById("row" + j + "col" + i).innerHTML = elementBelow;
           document.getElementById("row" + (j - 1) + "col" + i).innerHTML = "";
         }
         else{
           canMove = false;
+          
+          //if the numbers are the same then add them
+          if(element === elementBelow){
+            //add elements because they match
+            var sum = parseInt(element) + parseInt(elementBelow);
+            //put sum into the element above
+            document.getElementById("row" + j + "col" + i).innerHTML=sum;
+            //change element below with empty string
+            document.getElementById("row" + (j - 1) + "col" + i).innerHTML="";
+            
+          }  
         }
       }
     }
   }
 }
 
-//get random numbers
-function getRandomNum(min, max){
-  return Math.floor(Math.random() * max) + min;
+function moveLeft(){
+  
+  var canMove = true;
+  
+  while(canMove){
+    for(var j = 1; j < 4; j++){
+      for(var i = 1; i <= 4; i++){
+        var element = document.getElementById("row" + i + "col" + j).innerHTML;
+        var elementBelow = document.getElementById("row" + i + "col" + (j + 1)).innerHTML;
+
+      if(element === ""){
+          
+          document.getElementById("row" + i + "col" + j).innerHTML = elementBelow;
+          document.getElementById("row" + i + "col" + (j + 1)).innerHTML = "";
+        }
+        else{
+          canMove = false;
+          
+          //if the numbers are the same then add them
+          if(element === elementBelow){
+            //add elements because they match
+            var sum = parseInt(element) + parseInt(elementBelow);
+            //put sum into the element above
+            document.getElementById("row" + i + "col" + j).innerHTML=sum;
+            //change element below with empty string
+            document.getElementById("row" + i + "col" + (j + 1)).innerHTML="";
+            
+          }
+        }
+      }
+    }
+  }
 }
+
+function moveRight(){
+  
+  var canMove = true;
+  
+  while(canMove){
+    for(var j = 4; j > 1; j--){
+      for(var i = 4; i >= 1; i--){
+        var element = document.getElementById("row" + i + "col" + j).innerHTML;
+        var elementBelow = document.getElementById("row" + i + "col" + (j - 1)).innerHTML;
+
+      if(element === ""){
+          
+          document.getElementById("row" + i + "col" + j).innerHTML = elementBelow;
+          document.getElementById("row" + i + "col" + (j - 1)).innerHTML = "";
+        }
+        else{
+          canMove = false;
+          
+          //if the numbers are the same then add them
+          if(element === elementBelow){
+            //add elements because they match
+            var sum = parseInt(element) + parseInt(elementBelow);
+            //put sum into the element above
+            document.getElementById("row" + i + "col" + j).innerHTML=sum;
+            //change element below with empty string
+            document.getElementById("row" + i + "col" + (j - 1)).innerHTML="";
+            
+          }
+        }
+      }
+    }
+  }
+}
+
+function spawnNumberAfterTurn(){
+  
+  var row = getRandomNum(1,4);
+  var col = getRandomNum(1,4);
+  
+  var spawnNum = spawnNumber();
+  var location = 'row' + row + 'col' + col;
+  
+  document.getElementById(location).innerHTML=spawnNum;
+ 
+}
+
+
+
+
+
 
